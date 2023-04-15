7c2496de1948e88402cd0107c9c44f38f9416bf8
Rick <37203861+rickhehe@users.noreply.github.com>
2023-04-15 22:57:42 +1200

    Updated x.bat regarding changing directory.

--- x.bat
+++ x.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
-SETLOCAL ENABLEDELAYEDEXPANSION
+SETLOCAL enabledelayedexpansion
 
-SET CSVFOLDER="%cd%\csv"  ::PWD
+SET CSVFOLDER=%cd%
 
 SET SERVER=.\sqlexpress
 SET DB=rickdev
@@ -14,8 +14,9 @@ ECHO Last execution: %date% %time% > %OUTPUT%
 ::Recreate the table.
 SQLCMD -S %SERVER% -i sql\createTable.sql
 
-for %%f in (%CSVFOLDER%\*.csv) do (
-    set F=%%f
+echo Yes PM
+for %%f in (csv\*.csv) do (
+    set F=%CSVFOLDER%\%%f
     echo !F!
     sqlcmd -S %SERVER% -i sql\x.sql
 )
